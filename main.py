43f30b4bc24b7c43c4337f255c85536b5469427a
CodeCriminal365 <ytpizzacrafter@gmail.com>
2023-03-19 08:32:23 -0700

    More Questions

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def intro():
 
 def credits():
     print(" ")
-    print("Your Score Is:", score, "Out Of 13 Questions!")
+    print("Your Score Is:", score, "Out Of 24 Questions!")
     credits = open("credits.txt", "r")
     print(" ")
     print(credits.read())
@@ -23,27 +23,42 @@ categories = {
     "History": [
         {"question": "What year did World War I start?", "answer": "1914"},
         {"question": "Who was the first president of the United States?", "answer": "George Washington"},
-        {"question": "What year did the American Civil War end?", "answer": "1865"}
+        {"question": "What year did the American Civil War end?", "answer": "1865"},
     ],
     "Science": [
+        {"question": "This famous physicist developed the theory of relativity.", "answer": "Albert Einstein"},
         {"question": "What is the largest planet in our solar system?", "answer": "Jupiter"},
         {"question": "What is the smallest bone in the human body?", "answer": "Stapes"},
-        {"question": "What is the symbol for the element oxygen?", "answer": "O"}
+        {"question": "What is the symbol for the element oxygen?", "answer": "O"},
+        {"question": "This is the largest animal in the world.", "answer": "Blue Whale"},
     ],
     "Geography": [
         {"question": "What is the capital of Japan?", "answer": "Tokyo"},
         {"question": "What is the largest country in the world?", "answer": "Russia"},
-        {"question": "What is the smallest continent?", "answer": "Australia"}
+        {"question": "What is the smallest continent?", "answer": "Australia"},
+        {"question": "The Great Barrier Reef is located off the coast of this country.", "answer": "Australia"},
+        {"question": "What is the smallest country in the world by land area.", "answer": "Vatican City"},
+        {"question": "In which city would you find the famous statue of Christ the Redeemer?", "answer": "Rio de Janeiro"},
+        {"question": "This American city is known as the 'City of Brotherly Love.'", "answer": "Philadelphia"},
     ],
     "Games": [
         {"question": "What year did Markus 'Notch' Perrson make the video game Minecraft?", "answer": "2009"},
-        {"question": "When did the Nintendo Switch get released To the public?", "answer": "2017"}
+        {"question": "When did the Nintendo Switch get released To the public?", "answer": "2017"},
     ],
     "Famous People": [
-        {"question": "What is the date that Micheal Jackson died?", "answer": "June 25, 2009"}
+        {"question": "What is the date that Micheal Jackson died?", "answer": "June 25, 2009"},
+        {"question": "This famous physicist developed the theory of relativity.", "answer": "Albert Einstein"},
+        {"question": "This famous scientist discovered the law of gravity.", "answer": "Isaac Newton"},
     ],
     "Television": [
-        {"question": "What is the name of SpongeBob's best friend in the cartoon, SpongeBob SquarePants?", "answer": "Patrick Star"}
+        {"question": "What is the name of SpongeBob's best friend in the cartoon, SpongeBob SquarePants?", "answer": "Patrick Star"},
+    ],
+    "Movies": [
+        {"question": "This actor played the role of Forrest Gump in the 1994 film of the same name.", "answer": "Tom Hanks?"},
+    ],
+    "Books": [
+        {"question": "This author wrote 'To Kill a Mockingbird'.", "answer": "Harper Lee"},
+        {"question": "This author wrote 'The Catcher in the Rye.'", "answer": "J.D. Salinger"},
     ]
 }
 
